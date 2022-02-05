9df936345832011713ae09bcc282e2228c1ae70d
Vlad_Sm <vladislav.smekhnov@gmail.com>
2022-02-05 23:30:25 +0300

    Reload files and directories

--- Parser.py
+++ Parser.py
@@ -1,14 +1,19 @@
 import requests
 from bs4 import BeautifulSoup as BS
 import csv
+import pymysql
 
-CSV = 'Gamestop_results.csv'
+from config import host, user, password, db_name
+
+
+CSV = 'Stopgame_results.csv'
 HOST = 'https://stopgame.ru'
 URL = 'https://stopgame.ru/review/new/izumitelno/'
 HEADERS = {
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36'
 }
+
 
 def get_html(url, pageNumber, params=''):
     correct_url = url + str(pageNumber)
@@ -26,7 +31,7 @@ def get_content(html):
                 'title': item.find('div', class_='caption caption-bold').get_text(strip=True),
                 'review_link': HOST + item.find('div', class_='caption caption-bold').find('a').get('href'),
                 'brand': HOST,
-                'card_img': item.find('img', class_='image-16x9').get('src')
+                'image': item.find('img', class_='image-16x9').get('src')
             }
         )
     return games
@@ -37,7 +42,7 @@ def save_results_to_csv(items, path):
         writer = csv.writer(file, delimiter=';')
         writer.writerow(['Review name', 'Review link', 'Reviewer internet portal', 'Image link'])
         for item in items:
-            writer.writerow([item['title'], item['review_link'], item['brand'], item['card_img']]) # writing data to .csv
+            writer.writerow([item['title'], item['review_link'], item['brand'], item['image']])
 
 
 def parser():
@@ -60,3 +65,45 @@ def parser():
 
 
 parser()
+
+
+try:
+    connection = pymysql.connect(
+        host=host,
+        port=3306,
+        user=user,
+        password=password,
+        database=db_name,
+        cursorclass=pymysql.cursors.DictCursor
+    )
+    print("Successfully connected...")
+    try:
+        with connection.cursor() as cursor:
+            create_table_query = "CREATE TABLE `stopgame_db`(id int unsigned NOT NULL AUTO_INCREMENT," \
+                                 " title varchar(150)," \
+                                 " review_link varchar(256)," \
+                                 " brand varchar(256)," \
+                                 " image varchar(256), PRIMARY KEY (id));"
+            cursor.execute(create_table_query)
+            print("Table created successfully...")
+            with open(CSV) as csv_file:
+                csvfile = csv.reader(csv_file, delimiter=';')
+                all_values = []
+                for row in csvfile:
+                    value = (row[0], row[1], row[2], row[3])
+                    all_values.append(value)
+
+            query = "INSERT INTO `stopgame_db`(`title`, `review_link`, `brand`, `image`) VALUES (%s, %s, %s, %s)"
+            mycursor = connection.cursor()
+            mycursor.executemany(query, all_values)
+            connection.commit()
+    finally:
+        connection.close()
+except Exception as ex:
+    print("Connection refused...")
+    print(ex)
+
+
+
+
+
---
+++ config.py
@@ -0,0 +1,4 @@
+host = "127.0.0.1"
+user = "root"
+password = "14725"
+db_name = "test_py"
